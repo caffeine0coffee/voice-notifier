437c5944cd7067b870f8ff4d8ae99bf993541dcf
caffeine <kouta8145963@gmail.com>
2021-06-12 21:15:46 +0900

    feat: toggle delay system

--- main.py
+++ main.py
@@ -10,37 +10,87 @@ from discord.ext import commands, tasks
 PREFIX_TEXT = 'vn=='
 client = commands.Bot(command_prefix=PREFIX_TEXT)
 voice_member_count = {}
-notif_channel = {}
+
+# [
+#   guild_id: {
+#     "channel": Discord Channel object
+#     "send_time": Time to send the message (seconds since the epoch)
+#     "message": Message string
+#     "tag": "vc_start" / "vc_end"
+#   }, ...
+# ]
 message_queue = {}
+
+# [
+#   guild_id: {
+#     "notif_channel": channel_id
+#     "message_delay": True/False
+#   }, ...
+# ]
+guild_stat = {}
 
 isTest = False
 test_guild_id = None
 test_channel_id = None
 
 
-@client.command(help="set notification channel to where this command called")
+def update_guild_stat(gid, dic):
+    global guild_stat
+
+    json_data = {}
+    try:
+        with open("guild_status.json") as fin:
+            json_data = json.load(fin)
+    except FileNotFoundError as err:
+        print("{} is not exist.".format(err.filename))
+    
+    if json_data.get(str(gid)) is None:
+        json_data[str(gid)] = {}
+    for k, v in dic.items():
+        json_data[str(gid)][k] = v
+
+    with open("guild_status.json", mode='w') as fout:
+        json.dump(json_data, fout)
+    
+    guild_stat = json_data
+
+
+@client.command(help="Set notification channel to where this command called")
 async def set_channel(ctx):
-    notif_channel[ctx.guild.id] = ctx.channel
-
-    ids = {gid: c.id for gid,c in notif_channel.items()}
-    with open("notification_channel.json", mode='w') as fout:
-        json.dump(ids, fout)
-
+    update_guild_stat(ctx.guild.id, {"notif_channel": ctx.channel.id})
     await ctx.send("updated notification channel")
+
+
+@client.command(help="Toggle message delay")
+async def delay(ctx, arg=None):
+    if arg is None:
+        msg = ""
+        msg += "Usage: {}delay (on/off).\n".format(PREFIX_TEXT)
+        await ctx.send(msg)
+        return
+
+    flag = None
+    if arg == 'on' : flag = True
+    if arg == 'off': flag = False
+    if flag is None:
+        await ctx.send("Argument is invalid. Please specify 'on' or 'off'.")
+    else:
+        update_guild_stat(ctx.guild.id, {"message_delay": flag})
+        await ctx.send("Message delay is " + ("enabled" if flag else "disabled"))
 
 
 @client.event
 async def on_ready():
-    global notif_channel
+    global guild_stat
+
     print("logged in as {0.user}".format(client))
     await client.change_presence(activity=discord.Game(name=f"{PREFIX_TEXT}help"))
-    ids = {}
     try:
-        with open("notification_channel.json") as fin:
-            ids = json.load(fin)
+        with open("guild_status.json") as fin:
+            guild_stat = json.load(fin)
     except FileNotFoundError:
         pass
-    notif_channel = {int(gid): client.get_channel(cid) for gid,cid in ids.items()}
+
     check_message_queue.start()
 
 
@@ -52,7 +102,7 @@ async def check_message_queue():
     for gid, value in message_queue.items():
         if value["send_time"] < time.time():
             st = time.localtime(value["send_time"])
-            print("send a queue:")
+            print("send a queue message:")
             print(f"\ttime    : {st.tm_year}/{st.tm_mon}/{st.tm_mday} ", end='')
             print(f"{st.tm_hour}:{st.tm_min}.{st.tm_sec}")
             print("\tguild   : {}".format(value["channel"].guild.name))
@@ -75,13 +125,22 @@ async def on_voice_state_update(member, before, after):
         return
 
     guild = member.guild
-    guild_id = guild.id
-    print(datetime.datetime.now())
-    print("\tVC status update detected in guild '{}'".format(guild.name))
+    stat = guild_stat.get(str(guild.id))
 
-    if voice_member_count.get(guild_id) is None:
-        voice_member_count[guild_id] = 0
+    if stat is None:
+        channel_to_send = guild.system_channel
+        stat = {}
+    else:
+        channel_to_send = stat.get("notif_channel", guild.system_channel)
+    
+    if isTest:
+        channel_to_send = client.get_channel(test_channel_id)
 
+    # counter init
+    if voice_member_count.get(guild.id) is None:
+        voice_member_count[guild.id] = 0
+
+    # count up VC members
     num_vc_member = 0
     for vc in guild.voice_channels:
         members = client.get_channel(vc.id).members
@@ -89,32 +148,43 @@ async def on_voice_state_update(member, before, after):
         for m in members:
             print("\tdetected member: {}".format(str(m)))
 
-    print("\tprev count is {}, now is {}".format(voice_member_count[guild_id], num_vc_member))
+    # log
+    print(datetime.datetime.now())
+    print("\tVC status update detected in guild '{}'".format(guild.name))
+    print("\tprev count is {}, now is {}".format(voice_member_count[guild.id], num_vc_member))
 
-    channel_to_send = notif_channel.get(guild_id, guild.system_channel)
-    if voice_member_count[guild_id] == 0 and num_vc_member > 0:
-        print("\tadd VC started message queue (+10s)")
-        queue = {
-            "channel"   : channel_to_send,
-            "send_time" : time.time() + 10,
-            "message"   : "誰かが通話を始めたみたいです" + " [test]" if isTest else ""
-        }
-        message_queue[guild_id] = queue
+    msg = None
+    delay = 0
+    tag = ""
+    if voice_member_count[guild.id] == 0 and num_vc_member > 0:
+        delay = 10
+        msg = "誰かが通話を始めたみたいです"
+        tag = "vc_start"
         
-    elif voice_member_count[guild_id] > 0 and num_vc_member == 0:
-        if message_queue.get(guild_id) is None:
-            print("\tadd VC eed message queue")
+    elif voice_member_count[guild.id] > 0 and num_vc_member == 0:
+        if message_queue.get(guild.id) is None:
+            msg = "そして誰もいなくなった..."
+            tag = "vc_end"
+        elif stat.get("message_delay"):
+            print("\tVC is end, deleted message queue")
+            message_queue.pop(guild.id)
+
+    if msg is not None:
+        if isTest: msg += " [test]"
+        if stat.get("message_delay"):
             queue = {
                 "channel"   : channel_to_send,
-                "send_time" : time.time(),
-                "message"   : "そして誰もいなくなった..." + " [test]" if isTest else ""
+                "send_time" : time.time() + delay,
+                "message"   : msg,
+                "tag"       : tag
             }
-            message_queue[guild_id] = queue
+            message_queue[guild.id] = queue
+            print("added message queue: tag is {}, delay is +{}".format(tag, delay))
         else:
-            print("\tVC is end, deleted message queue")
-            message_queue.pop(guild_id)
+            await channel_to_send.send(msg)
+            print("send message: tag is {}".format(tag))
 
-    voice_member_count[guild_id] = num_vc_member
+    voice_member_count[guild.id] = num_vc_member
 
 
 if __name__ == "__main__":
